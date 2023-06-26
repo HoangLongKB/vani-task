e2d4c46ca9adb9295ff3004c5eeab76444ab7b48
HoangLongKB <hoanglongwork@gmail.com>
2023-06-27 04:09:30 +0700

    Add handle back button

--- src/components/quiz/quiz.tsx
+++ src/components/quiz/quiz.tsx
@@ -4,6 +4,7 @@ import { QuizQuestion } from '@/types/quiz';
 import React, { useEffect, useState } from 'react';
 import Question from './question';
 import Complete from './complete';
+import { usePathname, useRouter } from 'next/navigation';
 
 type Props = {
   quizQuestions: QuizQuestion[];
@@ -13,6 +14,55 @@ const Quiz = ({ quizQuestions }: Props) => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [isLastQuestion, setIsLastQuestion] = useState(false);
   const [showCompletionPage, setIsShowCompletePage] = useState(false);
+  const [unsavedChanges, setUnSavedChanges] = useState(true);
+
+  const pathName = usePathname();
+  const router = useRouter();
+
+  useEffect(() => {
+    window.history.pushState({curIndex: currentQuestionIndex}, '', '/quiz');
+  }, []);
+
+  useEffect(() => {
+    console.log('useEffect');
+    const controller = new AbortController();
+    const warningText =
+      'You have unsaved changes - are you sure you wish to leave this page?';
+    const handleWindowClose = (e: BeforeUnloadEvent) => {
+      console.log('handleWindowClose');
+      if (!unsavedChanges) return;
+      e.preventDefault();
+      return (e.returnValue = warningText);
+    };
+    const handleBackButton = (e: PopStateEvent) => {
+      console.log('handleBackButton e.state::::', e.state);
+      console.log('handleBackButton window.state::::', window.history);
+
+      var r = confirm('You pressed a Back button! Are you sure?!');
+
+      if (r == true) {
+        console.log('true');
+        window.history.back();
+        controller.abort();
+
+      } else {
+        console.log('false pathName::::', pathName);
+        console.log('false window.history::::', window.history);
+        window.history.pushState({curIndex: currentQuestionIndex}, '', '/quiz');
+
+        console.log('Stay on the current page.');
+      }
+    };
+    window.addEventListener('beforeunload', handleWindowClose);
+    window.addEventListener('popstate', handleBackButton, {
+      signal: controller.signal,
+    });
+    return () => {
+      console.log('removeEventListener');
+      window.removeEventListener('beforeunload', handleWindowClose);
+      window.removeEventListener('popstate', handleBackButton);
+    };
+  }, [unsavedChanges]);
 
   const handleChangeQuestion = (): void => {
     if (currentQuestionIndex === quizQuestions.length - 1) {
@@ -32,9 +82,7 @@ const Quiz = ({ quizQuestions }: Props) => {
   const currentQuestion = quizQuestions[currentQuestionIndex];
 
   if (showCompletionPage) {
-    return (
-      <Complete />
-    );
+    return <Complete />;
   }
 
   return (
